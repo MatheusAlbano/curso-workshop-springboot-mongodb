46b477de4bec7a22e11c3ed6aa18d7ff1b3d4f31
MatheusAlbano <albanoportomatheus@gmail.com>
2026-05-17 17:01:35 -0300

    Referenciando os posts dos usuários

--- src/main/java/com/matheusalbano/workshopmongo/domain/User.java
+++ src/main/java/com/matheusalbano/workshopmongo/domain/User.java
@@ -1,8 +1,12 @@
 package com.matheusalbano.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("user")
@@ -14,6 +18,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+    
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -47,4 +54,30 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 }
